e36ecaebdd2bb7c66f5ba6a70875dfcecb7e83e3
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-02 12:00:18 -0500

    Clase 131

--- ajax-ejercicios/assets/stripe-checkout.js
+++ ajax-ejercicios/assets/stripe-checkout.js
@@ -6,19 +6,41 @@ const $tacos = document.getElementById('tacos'),
 	$template = document.getElementById('taco-template').content,
 	$fragment = document.createDocumentFragment();
 
-fetch('https://api.stripe.com/v1/products', {
+const fetchOptions = {
 	headers: {
 		Authorization: `Bearer ${STRIPE_KEYS.secret}`,
 	},
-})
-	.then(response => response.json())
-	.then(console.log);
+};
 
-fetch('https://api.stripe.com/v1/prices', {
-	headers: {
-		Authorization: `Bearer ${STRIPE_KEYS.secret}`,
-	},
-})
-	.then(response => response.json())
-	.then(console.log)
-	.catch(console.log);
+let prices, products;
+
+Promise.all([
+	fetch('https://api.stripe.com/v1/products', fetchOptions),
+	fetch('https://api.stripe.com/v1/prices', fetchOptions),
+])
+	.then(responses => Promise.all(responses.map(response => response.json())))
+	.then(jsonResponse => {
+		console.log(jsonResponse);
+		products = jsonResponse[0].data;
+		prices = jsonResponse[1].data;
+		console.log('prices: ', prices, products);
+
+		for (const element of prices) {
+			const productData = products.filter(
+				product => product.id === element.product,
+			);
+			console.log(productData);
+
+			$template.querySelector('taco').setAttribute('data-price', element.id);
+			const clone = document.importNode($template, true);
+			$fragment.appendChild(clone);
+		}
+		$tacos.appendChild($fragment);
+	})
+	.catch(catchedError => {
+		console.log(catchedError);
+		const message =
+			catchedError.statusText ||
+			'Ocurrió un error al conectarse con la API de Stripe';
+		$tacos.innerHTML = `<p>Error ${catchedError.status}: ${message}</p>`;
+	});
