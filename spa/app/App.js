44daa14f2c35b2fdee8c27f1cca2b3ab20b023ed
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-12 11:55:42 -0500

    Empezando clase 147

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,5 +1,9 @@
+import api from './helpers/wp_api.js';
+
 export const App = function () {
 	document.getElementById(
 		'root',
 	).innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla.js</h1>`;
+
+	console.log(api);
 };
