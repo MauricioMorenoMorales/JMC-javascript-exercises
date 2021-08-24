6067f95f044c7d2aa349f8d9abb8622945fd08f6
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-24 10:01:39 -0500

    Empezando clase 152

--- spa/app/components/Menu.js
+++ spa/app/components/Menu.js
@@ -6,7 +6,11 @@ export const Menu = props => {
 		<span>-</span>
 		<a href="#/search">Busqueda</a>
 		<span>-</span>
+		<a href="#/contacto">Contacto</a>
+		<span>-</span>
 		<a href="https://aprendejavascript.org" target="_blank" rel="noopener">Aprende JS</a>
 	`;
 	return $menu;
 };
+
+('als ich wieder zu ');
--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -1,19 +1,30 @@
 import { ajax } from '../helpers/ajax.js';
-import api from '../helpers/wp_api.js'
+import api from '../helpers/wp_api.js';
 import { PostCard } from './PostCard.js';
 
 export const Router = function () {
+	const { hash } = location;
 
+	const $posts = document.getElementById('posts');
 
-	ajax({
-		url: api.POSTS,
-		successCallback: posts => {
-			console.log(posts);
-			let html = '';
-			posts.forEach(post => (html += PostCard(post)));
-			document.querySelector('.loader').style.display = 'none';
-			document.getElementById('posts').innerHTML = html;
-		},
-	});
+	$posts.innerHTML = null;
+
+	if (!hash || hash === '#/') {
+		ajax({
+			url: api.POSTS,
+			successCallback: posts => {
+				console.log(posts);
+				let html = '';
+				posts.forEach(post => (html += PostCard(post)));
+				document.querySelector('.loader').style.display = 'none';
+				$posts.innerHTML = html;
+			},
+		});
+	} else if (hash.includes('#/search')) {
+		$posts.innerHTML = `<h2>Sección del Buscador</h2>`;
+	} else if (hash === '#/contacto') {
+		$posts.innerHTML = `<h2>Sección de Contacto</h2>`;
+	} else {
+		$posts.innerHTML = `<h2>Aquí cargará el contenido de el Post previamente seleccionado</h2>`;
+	}
 };
-
