b11575361381e63ba5aa651f7c6e396016fe4d8c
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-24 10:57:13 -0500

    Minuto 9 clase 153

---
+++ spa/app/components/Post.js
@@ -0,0 +1,10 @@
+export const Post = props => `
+	<section class="post-page">
+		<aside>
+			<h2>Titulo de el Post</h2>
+			<time datetime="2020-11-12">12/11/2020</time>
+		</aside>
+		<hr/>
+		<article>Contenido de el Post</article>
+	</section>
+`;
--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -1,5 +1,6 @@
 import { ajax } from '../helpers/ajax.js';
 import api from '../helpers/wp_api.js';
+import { Post } from './Post.js';
 import { PostCard } from './PostCard.js';
 
 export const Router = async function () {
@@ -26,6 +27,16 @@ export const Router = async function () {
 		$main.innerHTML = `<h2>Sección de Contacto</h2>`;
 	} else {
 		$main.innerHTML = `<h2>Aquí cargará el contenido de el Post previamente seleccionado</h2>`;
+		// await ajax({
+		// 	url: api.POSTS,
+		// 	successCallback: main => {
+		// 		console.log(main);
+		// 		let html = '';
+		// 		main.forEach(post => (html += PostCard(post)));
+		// 		document.querySelector('.loader').style.display = 'none';
+		// 		$main.innerHTML = html;
+		// 	},
+		// });
 	}
 	document.querySelector('.loader').style.display = 'none';
 };
